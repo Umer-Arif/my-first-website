5fa848956708fcc5f94ee3c8a38e214c2b0f9fc9
madara123pain <161628269+madara123pain@users.noreply.github.com>
2026-02-22 15:25:16 +0500

    fix issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import homeBg from "../assets/bg.png";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
