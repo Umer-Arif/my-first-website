521a0ea4ee2b9944d05c893defbe845c83389a29
madara123pain <161628269+madara123pain@users.noreply.github.com>
2026-02-23 15:20:43 +0500

    logo fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import OmacsLogo from "../assets/OmacsLogo.svg";
+// 1. Add ?react to the end of the path
+import OmacsLogo from "../assets/OmacsLogo.svg?react";
 
 const Header: React.FC = () => {
   return (
-    <>
-      <div className="app-header">
-        <Link to="/" className="logo-link">
-          <img
-            src={OmacsLogo}
-            alt="Omacs Logo"
-            style={{ height: "80px", width: "auto" }}
-          />
-        </Link>
-        <nav>
-          <Link to="/"> Home </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/features"> Features </Link>
-          {/* <Link to="/contact"> Contact </Link> */}
-        </nav>
-      </div>
-    </>
+    <div className="app-header">
+      <Link to="/" className="logo-link">
+        <OmacsLogo style={{ height: "80px", width: "auto", color: "white" }} />
+      </Link>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/features"> Features </Link>
+      </nav>
+    </div>
   );
 };
 
